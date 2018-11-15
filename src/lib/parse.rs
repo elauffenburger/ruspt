f9545d4a6e6fa71ad9e597a576abe15d2f3a83d4
Eric Lauffenburger <Eric.Lauffenburger@technossus.com>
2018-11-14 16:53:20 -0800

    checkpoint on refactoring parsing

--- src/lib/parse.rs
+++ src/lib/parse.rs
@@ -15,71 +15,84 @@ pub fn parse(mut program: String) -> LispProgram {
 
     let entry = parse_init(&mut trimmed_program);
 
-    LispProgram { text: trimmed_program, entry: Some(Box::new(entry)) }
+    LispProgram {
+        text: trimmed_program,
+        entry: Some(Box::new(entry)),
+    }
 }
 
 fn parse_init(program: &mut String) -> LispCell {
     let mut sanitized_program = program.replace("(", " ( ").replace(")", " ) ");
     log(|| println!("sanitized_program: {:?}", &sanitized_program));
 
-    return parse_rec(&mut sanitized_program, ParseMode::Normal, &mut vec![], &mut String::new(), &mut vec![]);
+    let mut results = vec![];
+    parse_rec(&mut sanitized_program, true, &mut vec![], &mut String::new(), &mut results, 0);
+
+    println!("results: {:?}", &results);
+
+    return results.pop().unwrap();
 }
 
-fn parse_rec(text: &mut String, mode: ParseMode, list_stack: &mut Vec<char>, pending_word: &mut String, results: &mut Vec<LispCell>) -> LispCell {
-    log(|| println!("mode: {:?}", mode));
+fn parse_rec(text: &mut String, greedy: bool, list_stack: &mut Vec<char>, pending_word: &mut String, results: &mut Vec<LispCell>, depth: i32) {
+    log(|| println!("{}results: {:?}", tab_to_depth(depth), &results));
 
     if text.is_empty() {
-        return results.pop().unwrap();
+        // let contents = results.clone();
+        // results.push(LispCell::List {
+        //     contents: contents,
+        // });
+
+        return;
     }
 
     match text.remove(0) {
         ' ' | '\n' => {
-            log(|| println!("in whitespace"));
+            log(|| println!("{}in whitespace", tab_to_depth(depth)));
 
             // If there's no pending_word, just move onto the next char
             if pending_word == "" {
-                return parse_rec(text, mode, list_stack, pending_word, results);
+                return parse_rec(text, greedy, list_stack, pending_word, results, depth);
             }
 
-            log(|| println!("finalizing word: {}", &pending_word));
+            log(|| println!("{}finalizing word: {}", tab_to_depth(depth), &pending_word));
 
             let pending_word_str = pending_word.to_string();
 
             // Otherwise, close out this word and add it to the result set
             results.push(LispCell::Atom(pending_word_str));
 
-            // Move onto the next char
-            parse_rec(text, mode, list_stack, &mut String::new(), results)
+            if greedy {
+                // Move onto the next char
+                parse_rec(text, greedy, list_stack, &mut String::new(), results, depth);
+            }
         }
         '\'' => {
-            log(|| println!("in '"));
+            log(|| println!("{}in '", tab_to_depth(depth)));
 
-            let quoted = parse_rec(text, ParseMode::InList, list_stack, pending_word, results);
-
-            println!("to quote: {:?}", &quoted);
-
-            results.push(LispCell::Quoted(Box::new(quoted)));
-
-            parse_rec(text, mode, list_stack, &mut String::new(), results)
+            panic!("quote not implemented!");
         }
         '(' => {
             log(|| println!("in ("));
 
-            // Keep track of being inside this list
             list_stack.push('(');
 
-            // Get the contents of the list
-            let mut list = parse_rec(text, ParseMode::InFunc, list_stack, &mut String::new(), &mut vec![]);
+            log(|| println!("{}Staring new results stack", tab_to_depth(depth)));
 
-            results.push(list);
+            let mut list_contents = vec![];
+            parse_rec(text, greedy, list_stack, &mut String::new(), &mut list_contents, depth + 1);
 
-            log(|| println!("results: {:?}", &results));
+            log(|| println!("{}Finished results stack: {:?}", tab_to_depth(depth), &list_contents));
 
-            // Move onto the next token in program
-            parse_rec(text, mode, list_stack, &mut String::new(), results)
+            results.push(LispCell::List {
+                contents: list_contents,
+            });
+
+            if greedy {
+                parse_rec(text, greedy, list_stack, &mut String::new(), results, depth);
+            }
         }
         ')' => {
-            log(|| println!("in )"));
+            log(|| println!("{}in )", tab_to_depth(depth)));
 
             if list_stack.is_empty() {
                 // TODO: handle this better
@@ -87,30 +100,23 @@ fn parse_rec(text: &mut String, mode: ParseMode, list_stack: &mut Vec<char>, pen
             }
 
             list_stack.pop();
-
-            log(|| println!("list contents: {:?}", &results));
-            log(|| println!("mode: {:?}", &mode));
-
-            match results.as_slice() {
-                [] => LispCell::List { contents: vec![] },
-                _ => match mode {
-                    ParseMode::Normal | ParseMode::InList | ParseMode::InFunc => LispCell::List { contents: results.clone() },
-                    _ => panic!("Unsupported eval mode!"),
-                },
-            }
         }
         '"' => {
             // TODO: handle, you know, strings
             panic!("Strings not supported yet!")
         }
         c @ _ => {
-            log(|| println!("c: {}", &c));
+            log(|| println!("{}c: {}", tab_to_depth(depth), &c));
 
             // We're either starting or adding to a pending word
             pending_word.push(c);
 
             // ...either way, we just continue
-            parse_rec(text, mode, list_stack, pending_word, results)
+            parse_rec(text, greedy, list_stack, pending_word, results, depth)
         }
     }
+}
+
+fn tab_to_depth(depth: i32) -> String {
+    format!("({}):{}", depth, "  ".repeat(depth as usize).to_string())
 }
