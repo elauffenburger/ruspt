588c23095380f43ff141d7b51ab7a2e8a68da07a
Eric Lauffenburger <elauffenburger@chipotle.com>
2018-11-20 11:34:28 -0800

    * add commandline args to bin

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,39 @@
 extern crate rusptlib;
 
 use rusptlib::{exec_prog, parse, Environment};
+use std::env;
 use std::io::{self, Write};
 
+enum RunMode {
+    Repl,
+    Server,
+}
+
 fn main() {
-    print!("Welcome to ruspt!");
+    let mut args = env::args().skip(1);
+    let mut run_mode = None;
+
+    loop {
+        let arg = args.next();
+
+        match arg {
+            Some(arg) => match arg.as_str() {
+                "--repl" => run_mode = Some(RunMode::Repl),
+                "--server" => run_mode = Some(RunMode::Server),
+                _ => panic!("Unknown option {:?}", arg),
+            },
+            None => break,
+        };
+    }
+
+    match run_mode {
+        None | Some(RunMode::Repl) => repl(),
+        Some(RunMode::Server) => server(),
+    }
+}
+
+fn repl() {
+    println!("Welcome to ruspt!");
 
     let mut env = Environment::new();
 
@@ -20,4 +49,8 @@ fn main() {
 
         println!("{:?}", result);
     }
+}
+
+fn server() {
+    print!("Starting server...");
 }
